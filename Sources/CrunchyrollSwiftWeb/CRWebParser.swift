cee3b5e9147baab3920d25cd9cf67627a2666398
HackingGate <i@hackinggate.com>
2020-09-24 21:57:32 +0900

    Add CRNameParser

--- Sources/CrunchyrollSwiftWeb/CRWebParser.swift
+++ Sources/CrunchyrollSwiftWeb/CRWebParser.swift
@@ -9,6 +9,13 @@ import Foundation
 import Kanna
 
 public struct CRWebParser {
+    static let baseURL = "https://www.crunchyroll.com/"
+    public static func seriesId(_ nameToken: String) -> Int? {
+        if let url = URL(string: baseURL + nameToken) {
+            return seriesId(url)
+        }
+        return nil
+    }
     public static func seriesId(_ url: URL) -> Int? {
         if let doc = try? HTML(url: url, encoding: .utf8) {
             // Search for nodes by CSS
