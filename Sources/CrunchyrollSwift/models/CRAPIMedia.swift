e3cdb20b650a412ca4b8e1830d1c89692e483310
HackingGate <i@hackinggate.com>
2020-12-14 22:43:03 +0900

    Support screenshot_image

--- Sources/CrunchyrollSwift/models/CRAPIMedia.swift
+++ Sources/CrunchyrollSwift/models/CRAPIMedia.swift
@@ -9,23 +9,25 @@ import Foundation
 
 public struct CRAPIMedia: Codable, Identifiable {
     public let id: String // media_id
-    public let collectionId: String
-    public let episodeNumber: String
-    public let name: String
-    public let description: String
-    public let url: String
-    public let seriesName: String
-    public let collectionName: String
-    public let premiumOnly: Bool
+    public let collectionId: String?
+    public let episodeNumber: String?
+    public let name: String?
+    public let description: String?
+    public let screenshotImage: CRAPIMediaScreenshotImage?
+    public let url: String?
+    public let seriesName: String?
+    public let collectionName: String?
+    public let premiumOnly: Bool?
     public var streamData: CRAPIStreamData?
     public let playhead: Int?
     
-    enum CodingKeys: String, CodingKey {
+    public enum CodingKeys: String, CodingKey {
         case id = "media_id"
         case collectionId = "collection_id"
         case episodeNumber = "episode_number"
         case name = "name"
         case description = "description"
+        case screenshotImage = "screenshot_image"
         case url = "url"
         case seriesName = "series_name"
         case collectionName = "collection_name"
---
+++ Sources/CrunchyrollSwift/models/CRAPIMediaScreenshotImage.swift
@@ -0,0 +1,22 @@
+//
+//  CRAPIMediaScreenshotImage.swift
+//  
+//
+//  Created by HG on 2020/12/14.
+//
+
+import Foundation
+
+public struct CRAPIMediaScreenshotImage: Codable {
+    public let thumb_url: String
+    public let small_url: String
+    public let medium_url: String
+    public let large_url: String
+    public let full_url: String
+    public let wide_url: String
+    public let widestar_url: String
+    public let fwide_url: String
+    public let fwidestar_url: String
+    public let width: String
+    public let height: String
+}
