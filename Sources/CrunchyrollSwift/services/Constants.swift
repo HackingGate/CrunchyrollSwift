f7a2b013f239ede32f6a504fa73ffbcf1cac7980
HackingGate <i@hackinggate.com>
2021-07-12 20:36:37 +0900

    Fix swiftlint

--- Sources/CrunchyrollSwift/services/Constants.swift
+++ Sources/CrunchyrollSwift/services/Constants.swift
@@ -2,7 +2,7 @@ import Foundation
 
 extension CRAPIService {
     public enum Constants {
-      static let BASE_URL = URL(string: "https://api.crunchyroll.com/")!
-      static let CR_UNBLOCKER_URL = URL(string: "https://cr-unblocker.us.to")!
+      static let BaseURL = URL(string: "https://api.crunchyroll.com/")!
+      static let crUnblockerURL = URL(string: "https://cr-unblocker.us.to")!
     }
 }
